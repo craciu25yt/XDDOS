8f9039024aeaee6f4d75ad3602ef7840d2593098
GamingLPyt <lars26122002@gmail.com>
2022-05-01 01:52:07 +0200

    Hehe

--- src/main/java/de/xddosapi/repository/IXddos.java
+++
@@ -1,7 +0,0 @@
-package de.xddosapi.repository;
-
-import de.xddosapi.entity.XddosEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IXddos extends JpaRepository<XddosEntity, Long> {
-}
--- src/main/java/de/xddosapi/utils/enums/Protocol.java
+++
@@ -1,4 +0,0 @@
-package de.xddosapi.utils.enums;
-
-public enum Protocol {
-}
