16c5c8e1235b665942d32960f53db10d30da252b
MEOW <84959276+XDMEOW@users.noreply.github.com>
2022-05-01 05:11:23 +0530

    Delete src/main/java/XD/XDDOS directory

--- src/main/java/XD/XDDOS/methods/Method.java
+++
@@ -1,7 +0,0 @@
-package XD.XDDOS.methods;
-
-import XD.XDDOS.ProxyLoader;
-import io.netty.channel.Channel;
-import java.util.function.BiConsumer;
-
-public interface Method extends BiConsumer<Channel, ProxyLoader.Proxy> {}
